6b209bae74fe7ae46ab28c53d157bcf1379eea06
joneidejohnsen <joneidejohnsen@gmail.com>
2016-03-08 12:56:09 +0100

    La til riktig kode

---
+++ src/Prosjekt/Hovedprogram.java
@@ -0,0 +1,16 @@
+package Prosjekt;
+
+public class Hovedprogram {
+
+	public static void main(String[] args) {
+		DatabaseConnection dbconn = new DatabaseConnection();
+		DBOperations operations = new DBOperations(dbconn.getConnection());
+		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
+		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
+		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
+		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
+		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
+		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
+		System.out.println(operations.showAll());
+	}
+}
