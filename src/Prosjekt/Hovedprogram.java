2cf52ecafa39de2aaf5f7a615dceb6415e409e3e
Vemund Wøien <vemundwo@gmail.com>
2016-03-08 14:05:21 +0100

    fix

--- src/Prosjekt/Hovedprogram.java
+++ src/Prosjekt/Hovedprogram.java
@@ -1,17 +1,21 @@
 package Prosjekt;
+import java.util.Scanner;
 
 public class Hovedprogram {
 
 	public static void main(String[] args) {
 		DatabaseConnection dbconn = new DatabaseConnection();
-		DBOperations operations = new DBOperations(dbconn.getConnection());
-		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
-		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
-		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
-		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
-		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
-		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
-		System.out.println(operations.showAll());
-		dbconn.closeConnection();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the Personal WorkOutDiary©. Please choose the wanted action: \n\n   1. Make diary\n   2. Register workout\n   3. Register excersice\n   4. Register goal\n   5. Manage categories\n   6. Get info");
+		while(sc.hasNext()){
+			int action = sc.nextInt();
+			switch (action) {
+			case 1: MakeDiary d = new MakeDiary(dbconn.getConnection());
+					d.run();
+			}
+			
+		}
+		
+		//dbconn.closeConnection();
 	}
 }
