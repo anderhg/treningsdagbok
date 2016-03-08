d54e901e9f2db2f7665b803a4c4dc15d23ac48c0
Vemund Wøien <vemundwo@gmail.com>
2016-03-08 16:29:53 +0100

    fix

--- src/Prosjekt/Hovedprogram.java
+++ src/Prosjekt/Hovedprogram.java
@@ -4,18 +4,32 @@ import java.util.Scanner;
 public class Hovedprogram {
 
 	public static void main(String[] args) {
+		boolean Con = true;
 		DatabaseConnection dbconn = new DatabaseConnection();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Welcome to the Personal WorkOutDiary©. Please choose the wanted action: \n\n   1. Make diary\n   2. Register workout\n   3. Register excersice\n   4. Register goal\n   5. Manage categories\n   6. Get info");
-		while(sc.hasNext()){
+		System.out.println("Welcome to the Personal WorkOutDiary©. Please choose the wanted action: \n\n   1. Make diary\n   2. Register workout\n   3. Register excersice\n   4. Register goal\n   5. Manage categories\n   6. Get info\n   7. Exit");
+		while(Con){
+			Scanner sc = new Scanner(System.in);
 			int action = sc.nextInt();
 			switch (action) {
 			case 1: MakeDiary d = new MakeDiary(dbconn.getConnection());
 					d.run();
+			case 2: registerWorkout w = new registerWorkout(dbconn.getConnection());
+					w.run();
+			case 3: registerExercise e  = new registerExercise(dbconn.getConnection());
+					e.run();
+			case 4: registerGoal g = new registerGoal(dbconn.getConnection());
+					g.run();
+			case 5: manageCategories c = new manageCategories(dbconn.getConnection());
+					c.run();
+			case 6: getInfo i = new getInfo(dbconn.getConnection());
+					i.run();
+			case 7: Con = false; 
+					//dbconn.closeConnection();
+			
 			}
+			
 			
 		}
 		
-		//dbconn.closeConnection();
 	}
 }
---
+++ src/Prosjekt/registerGoal.java
@@ -0,0 +1,16 @@
+package Prosjekt;
+
+import java.sql.Connection;
+
+public class registerGoal {
+
+	public registerGoal(Connection connection) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
