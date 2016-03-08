7108002d05a58c1c91a540db923859112df45f89
anderhg <andersgeitle@gmail.com>
2016-03-08 19:34:50 +0100

    Fikset registerExercise og nesten ferdig med registerWorkout

--- src/Prosjekt/Hovedprogram.java
+++ src/Prosjekt/Hovedprogram.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Hovedprogram {
 
 	public static void main(String[] args) {
+		int diaryID = 0;
 		boolean Con = true;
 		DatabaseConnection dbconn = new DatabaseConnection();
 		System.out.println("Welcome to the Personal WorkOutDiary©. Please choose the wanted action: \n\n   1. Make diary\n   2. Register workout\n   3. Register excersice\n   4. Register goal\n   5. Manage categories\n   6. Get info\n   7. Exit");
@@ -13,7 +14,7 @@ public class Hovedprogram {
 			switch (action) {
 			case 1: MakeDiary d = new MakeDiary(dbconn.getConnection());
 					d.run();
-			case 2: registerWorkout w = new registerWorkout(dbconn.getConnection());
+			case 2: registerWorkout w = new registerWorkout(dbconn.getConnection(), diaryID);
 					w.run();
 			case 3: registerExercise e  = new registerExercise(dbconn.getConnection());
 					e.run();
