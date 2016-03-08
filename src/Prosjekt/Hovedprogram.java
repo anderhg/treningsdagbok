69d380a32ff9ba5d513302d0d66520d911959a41
anderhg <andersgeitle@gmail.com>
2016-03-08 13:34:54 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	bin/Prosjekt/DatabaseConnection.class
    #	bin/Prosjekt/Hovedprogram.class
    #	src/Prosjekt/DatabaseConnection.java
    #	src/Prosjekt/Hovedprogram.java
    #	src/Prosjekt/VemundSkriver.java

--- src/Prosjekt/Hovedprogram.java
+++ src/Prosjekt/Hovedprogram.java
@@ -5,12 +5,8 @@ public class Hovedprogram {
 	public static void main(String[] args) {
 		DatabaseConnection dbconn = new DatabaseConnection();
 		DBOperations operations = new DBOperations(dbconn.getConnection());
-		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
-		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
-		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
-		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
-		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
-		operations.insertExercise("Markløft", "Baseøvelse for rygg/ben");
+		operations.insertExercise("Skråbenk", "Øvelse for skuldre/bryst");
+		
 		System.out.println(operations.showAll());
 	}
 }
