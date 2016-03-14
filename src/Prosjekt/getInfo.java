bfed9e3cb5926d34a2e6bc6f761e3b66902e6d02
joneidejohnsen <joneidejohnsen@gmail.com>
2016-03-14 10:44:08 +0100

    Oppdatert getInfo

--- src/Prosjekt/getInfo.java
+++ src/Prosjekt/getInfo.java
@@ -1,16 +1,40 @@
 package Prosjekt;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class getInfo {
-
-	public getInfo(Connection connection) {
-		// TODO Auto-generated constructor stub
+	
+	private Statement myStmt;
+	private Connection myConn;
+	private int bokId;
+	
+	public getInfo(Connection conn, int bokId) {
+		myConn = conn;
+	}
+	
+	//Get-metoder
+	
+	public String getLog(){
+		String ret = "";
+		try {
+			myStmt = myConn.createStatement();
+			ResultSet myRs = myStmt.executeQuery("Select * from treningsøkt where Treningsdagbok_BokId=" + bokId);
+			while (myRs.next()){
+			ret += "" + myRs.getString("Dato") + ": " + myRs.getString("Notat") + "\n";
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		return ret; 
+			
 	}
 
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		System.out.println(getLog());
 	}
 
 }
